b23423cfa0ca0bc1cd6af852aeeaab75f1b694a4
Bohdan Forostianyi <bohdanforostianyi@gmail.com>
2021-01-05 00:18:34 +0100

    Added cookies to request, fixed mistake field in UserModel, fixed url

--- projects/login-lib/src/lib/auth/auth.service.ts
+++ projects/login-lib/src/lib/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { interval, Observable, of, Subscription } from 'rxjs';
-import { catchError, first, map, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 interface ServerUserModel {
   is_authenticated: boolean;
@@ -20,54 +20,57 @@ interface ServerTokenResponse {
   providedIn: 'root',
 })
 export class AuthService {
-  private _accessToken: string = "";
+  private _accessToken: string = '';
   public get accessToken(): string {
     return this._accessToken;
   }
 
   private get callbackUrl() {
-    const base = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
+    const base = window.location.href.substring(
+      0,
+      window.location.href.lastIndexOf('/')
+    );
     return `${base}/callback-url`;
   }
 
   connectionExists: boolean = false;
-  private clientId = '123123';
   private identityServerUrl = 'http://localhost:8000';
+  // TODO: Add provider for this values
+  private clientId = '123123';
   private requiredScope = ['read adminpanel', 'write adminpanel'];
 
   private currentInterval = 200;
   private source = interval(this.currentInterval);
 
-
-  private pingSubscription:Subscription;
+  private pingSubscription: Subscription;
 
   constructor(private http: HttpClient, public router: Router) {
-
-    this.pingSubscription=this.pingServer();
-    
-
-
+    this.pingSubscription = this.pingServer();
   }
 
-  private pingServer(): Subscription
-  {
+  private pingServer(): Subscription {
     return this.source.subscribe(() => {
-      this.http.get(`${this.identityServerUrl}/user`, { observe: 'response' })
-        .subscribe(resp => { this.connectionExists = resp.status === 200;  this.pingSubscription.unsubscribe(); },err=>
-        {
-          this.pingSubscription.unsubscribe();
-          this.currentInterval+=5000;
-          this.source=interval(this.currentInterval);
-          this.pingSubscription=this.pingServer();
-        }
+      this.http
+        .get(`${this.identityServerUrl}/is_active`, { observe: 'response' })
+        .subscribe(
+          (resp) => {
+            this.connectionExists = resp.status === 200;
+            this.pingSubscription.unsubscribe();
+          },
+          (err) => {
+            this.pingSubscription.unsubscribe();
+            this.currentInterval += 5000;
+            this.source = interval(this.currentInterval);
+            this.pingSubscription = this.pingServer();
+          }
         );
-      
-
-    })
+    });
   }
   public isAuthenticated(): Observable<UserModel> {
     return this.http
-      .get<ServerUserModel>(`${this.identityServerUrl}/is_authenticated`)
+      .get<ServerUserModel>(`${this.identityServerUrl}/is_authenticated`, {
+        withCredentials: true,
+      })
       .pipe(
         map((response) => ({
           isAuthenticated: response.is_authenticated,
@@ -77,11 +80,7 @@ export class AuthService {
   }
 
   public authenticate() {
-    this.router.navigateByUrl(
-      this.router.createUrlTree(
-        ['/callback-url'], {}
-      )
-    );
+    this.router.navigateByUrl(this.router.createUrlTree(['/callback-url'], {}));
     const url = `${this.identityServerUrl}/login?callback_url=${this.callbackUrl}&scope=${this.requiredScope}&client_id=${this.clientId}`;
     window.location.replace(url);
   }
@@ -90,7 +89,7 @@ export class AuthService {
     const url = `${this.identityServerUrl}/token?code=${code}`;
     return this.http.get<ServerTokenResponse>(url).pipe(
       map((token) => token.token),
-      tap((token) => this._accessToken = token)
+      tap((token) => (this._accessToken = token))
     );
   }
 }
